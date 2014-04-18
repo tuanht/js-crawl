c3a0caaf91a2e8b6d51d141c5358561751591f99
Tuan Ha <tuanht.unix@gmail.com>
2014-04-19 00:51:02 +0700

    Validate value when input page URL. Also add function to set label that indicate the status of crawl request

--- js/view/HomePageView.js
+++ js/view/HomePageView.js
@@ -21,9 +21,44 @@ HomePageView = $class(BaseView, {
      * Get String input in `crawlUrl` text box
      *
      * @return URL string
+     * @throw Error Input is empty or not a URL
      */
     getCrawlUrl: function() {
         var crawlUrlInput = $id("crawlUrl");
+        this.validateUrl(crawlUrlInput.value);
         return crawlUrlInput.value;
+    },
+
+    /**
+     * Validate input is empty or not a URL
+     *
+     * @throw Error when Input is empty or not a URL
+     */
+    validateUrl: function(url) {
+        if (url == "") {
+            throw "Please enter a page URL";
+        }
+
+        var location = getLocationFromUrlString(url);
+
+        if (location.hostname == "" || location.hostname.indexOf(".") == -1) {
+            throw "You must enter a valid page URL";
+        }
+
+        return true;
+    },
+
+    /**
+     * Set label that indicate the status of crawl request
+     *
+     * @param status String message. This message also used to add class name
+     *     to stylish
+     */
+    setCrawlStatusLabel: function(status) {
+        var label = $id("crawlStatusLabel");
+        if (label) {
+            $setText(label, status);
+            label.className = status.toLowerCase();
+        }
     }
 });
